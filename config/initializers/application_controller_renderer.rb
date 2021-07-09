d38e078be0c97aff7776b826e940a4f7a2996b05
AMI-KATSUMATA <ec2-user@ip-172-31-6-224.ec2.internal>
2021-07-09 15:04:09 +0000

    [finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cace1c0dd3652d66df8af891a89fb7b4b36b3dc91fc909063de0c4994464f6e0e016fffe20fc09aef185d9a32c1391ff66d4d64dcedf97e107cde43b66277e06'
